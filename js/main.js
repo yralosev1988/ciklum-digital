2d2475ab5ec253ae3802fd1a787e82be109d0ec2
yralosev1988 <yralosev1988@gmail.com>
2019-10-25 21:26:05 +0300

    add some js b scss functions

--- js/main.js
+++ js/main.js
@@ -1,39 +1,55 @@
 'use strict';
-let showPopup = document.getElementById('showPopup');
-let hidePopup = document.getElementById('hidePopup');
+let openModal = document.getElementById('openModal');
+let closeModal = document.getElementById('closeModal');
+let modal = document.getElementById('modal');
+
+openModal.onclick=function(event){
+  event.preventDefault();
+  modal.style.display='block';
+};
+closeModal.onclick=function(event){
+  event.preventDefault();
+  let modal=document.getElementById('modal');
+  modal.style.display='none';
+};
+window.onclick=function(event){
+  if(event.target===modal){
+    modal.style.display='none';
+  }
+};
+
+
 let createTask = document.getElementById('createTask');
 let title = document.getElementById('title');
 let description = document.getElementById('description');
 let item__priority = document.getElementById('item__priority');
 
-let tasks = document.getElementById('tasks');
-let item__title = document.getElementById('item__title');
-let item__description = document.getElementById('item__description');
-let item__prior = document.getElementById('item__prior');
-let ok=document.getElementById('ok');
-let edit=document.getElementById('edit');
-let del=document.getElementById('del');
-let hidePop=document.getElementById('hidePop');
+let tasks__item = document.querySelector('.tasks__item');
+let item__title = document.querySelector('.item__title');
+let item__description = document.querySelector('.item__description');
+let item__prior = document.querySelector('.item__prior');
 
-showPopup.onclick=function(event){
-  event.preventDefault();
-  let popup=document.getElementById('popup');
-  popup.style.display='block';
-};
-hidePopup.onclick=function(event){
-  event.preventDefault();
-  let popup=document.getElementById('popup');
-  popup.style.display='none';
-};
+
+let ok=document.querySelector('.ok');
+let edit=document.querySelector('.edit');
+let del=document.querySelector('.del');
+let showConfig=document.querySelector('.showConfig');
+
+
 
 createTask.onclick=function (event) {
   event.preventDefault();
+  event.stopPropagation();
+
   item__title.innerText=title.value;
   item__description.innerText=description.value;
   item__prior.innerText=item__priority.value;
-  tasks.style.display='block';
-  let popup=document.getElementById('popup');
-  popup.style.display='none';
+  tasks__item.style.display='block';
+  let modal=document.getElementById('modal');
+  modal.style.display='none';
+  let tasks__item2= tasks__item.cloneNode(true);
+  tasks__item2.querySelector('h2').innerHTML='NEW ITEM2';
+  tasks__item.after(tasks__item2);
 /*
   alert(event);
   alert(title.value);
@@ -42,34 +58,45 @@ createTask.onclick=function (event) {
   alert(142);
 */
 };
-hidePop.onclick=function(event){
+showConfig.onclick=function(event){
   event.preventDefault();
-  let showPop=document.getElementById('showPop');
-
+  let showPop=document.querySelector('.showPop');
   showPop.style.display='block';
-
 };
+window.onclick=function(event){
+  event.preventDefault();
+  if(event.target!==showConfig){
+    showPop.style.display='none';
+  }
+};
+
+
+
+let showPop=document.querySelector('.showPop');
+
+
 ok.onclick=function(event){
   event.preventDefault();
-  event.stopPropagation();
-  let showPop=document.getElementById('showPop');
-
   showPop.setAttribute('style', 'display: none ;');
-
-  tasks.style.backgroundColor='blue';
+  tasks__item.style.backgroundColor="blue";//rework event
+  event.stopPropagation();
 
 };
 edit.onclick=function(event){
   event.preventDefault();
   event.stopPropagation();
-  let showPop=document.getElementById('showPop');
+  let showPop=document.querySelector('.showPop');
   showPop.setAttribute('style', 'display: none ;');
-  let popup=document.getElementById('popup');
-  popup.style.display='block';
+  modal.style.display='block';
 };
 del.onclick=function(event){
   event.preventDefault();
   event.stopPropagation();
-  tasks.style.display='none';
+  tasks__item.style.display='none';
 };
 
+
+
+/*let tasks__item2= tasks__item.cloneNode(true);
+tasks__item2.querySelector('h2').innerHTML='NEW ITEM2';
+tasks__item.after(tasks__item2);*/
