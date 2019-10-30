699ba508314b210622bdfc550b183ef3d01f0392
yralosev1988 <yralosev1988@gmail.com>
2019-10-31 00:36:55 +0300

    config webpack+eslint airbnb+ scss styles rework

--- src/js/app.js
+++ src/js/app.js
@@ -1,80 +1,93 @@
 import '../scss/app.scss';
 
-// Your JS Code goes here
-'use strict';
-let createTaskInModal = document.getElementById('createTaskInModal');//button witch create task in modal
-let closeModal = document.getElementById('closeModal');//button witch close modal
-let modal = document.getElementById('modal');//modal
-let createTask = document.getElementById('createTask');//button witch create/save task depending on flag create/save in dataset
-
-//listener to show modal and set flag "create"
-
-createTaskInModal.addEventListener('click', function (event) {
-  event.preventDefault();
-  showModal();
-  setFlag('create');
-});
+const createTaskInModal = document.getElementById('createTaskInModal');// button witch create task in modal
+const closeModal = document.getElementById('closeModal');// button witch close modal
+const modal = document.getElementById('modal');// modal
+const createTask = document.getElementById('createTask');// button witch create/save task depending on flag create/save in dataset
+const searchFilter = document.getElementById('searchFilter');
+const statusFilter = document.getElementById('statusFilter');
+const priorityFilter = document.getElementById('priorityFilter');
+const tasks = document.getElementById('tasks'); // element to render tasks
+const titleModal = document.getElementById('title'); // title from modal
+const titleDescription = document.getElementById('description');// description modal
+const titlePriority = document.getElementById('priority');// priority from modal
+// global variables to save values from targ obj
+let itemTitle;
+let itemDescription;
+let itemPrior;
 
 function showModal() {
   modal.style.display = 'block';
 }
 
-//set flag "create"/"save"
-function setFlag(flag) {
-  createTask.dataset.condition = flag;
-}
-
-//listener to close modal by click on button
-
-closeModal.addEventListener('click', function (event) {
-  event.preventDefault();
-  hideModal();
-});
-
 function hideModal() {
   modal.style.display = 'none';
 }
 
-//listener to close modal by click on dark layout
+// set flag "create"/"save"
+function setFlag(flag) {
+  createTask.dataset.condition = flag;
+}
 
-window.addEventListener('click', function (event) {
+// NEW FILTER
+function filter() {
+  // get ARRAY with tasks DOM-objects
+  const ARRAY = document.querySelectorAll('.tasks__item');
+  // get value of input
+  const searchValue = searchFilter.value;
+  // get value of status select
+  const statusValue = statusFilter.value;
+  // get value of priority select
+  const priorityValue = priorityFilter.value;
+  // get values to compare with selects and inputs
+  ARRAY.forEach((element) => {
+    const elem = element;
+    // each element in array compare with values in tasks
+    if (elem.querySelector('.itemTitle')
+            .innerHTML
+            .includes(searchValue)
+        && (elem.dataset.id === statusValue || statusValue === 'all')
+        && (elem.querySelector('.itemPrior').innerHTML === priorityValue || priorityValue === 'all')) {
+      // do something
+      elem.style.display = 'block';
+    } else {
+      elem.style.display = 'none';
+    }
+  });
+}
+
+// listener to show modal and set flag "create"
+createTaskInModal.addEventListener('click', (event) => {
+  event.preventDefault();
+  showModal();
+  setFlag('create');
+});
+// listener to close modal by click on button
+closeModal.addEventListener('click', (event) => {
+  event.preventDefault();
+  hideModal();
+});
+// listener to close modal by click on dark layout
+window.addEventListener('click', (event) => {
   if (event.target === modal) {
     hideModal();
   }
 });
 
-
-//kajsdkashdk
-
-let tasks = document.getElementById('tasks'); //element to render tasks
-let titleModal = document.getElementById('title'); //title from modal
-let titleDescription = document.getElementById('description');//description modal
-let titlePriority = document.getElementById('priority');//priority from modal
-
-let showPop = document.querySelector('.showPop');
-
-
-//глобальные переменные для хранения состояния this
-let item__title;
-let item__description;
-let item__prior;
-
-//value of input
-
-
-createTask.addEventListener('click', function (event) {
+// value of input
+createTask.addEventListener('click', (event) => {
   event.preventDefault();
-  let titleModalValue = titleModal.value; //title value from modal
-  let descriptionModalValue = titleDescription.value;//description value from modal
-  let priorityModalValue = titlePriority.value;//priority value from modal
-  //task item view to dynamic add in dom
-
-  let taskNode = `<div class="tasks__item" data-id="open">
-    <h2 class="item__title">${titleModalValue}</h2>
-  <p class="item__description">${descriptionModalValue}</p>
-  <div class="wrapper grid2">
-  <div class="item__prior">${priorityModalValue}</div>
-  <div class="showConfig">
+  const titleModalValue = titleModal.value; // title value from modal
+  const descriptionModalValue = titleDescription.value;// description value from modal
+  const priorityModalValue = titlePriority.value;// priority value from modal
+  // task item view to dynamic add in dom
+  const taskNode = `<div class="tasks__item" data-id="open">
+<div class="complete"></div>
+    <h2 class="itemTitle">${titleModalValue}</h2>
+  <p class="itemDescription">${descriptionModalValue}</p>
+  <div class="wrapper">
+  <div class="itemPrior">${priorityModalValue}</div>
+  <div class="showConfig">...
   <div class="showPop">
   <div class="ok">done</div>
   <div class="edit">edit</div>
@@ -84,107 +97,53 @@ createTask.addEventListener('click', function (event) {
   </div>
   </div>`;
   if (createTask.dataset.condition === 'create') {
-    //create in DOM new task
+    // create in DOM new task
     tasks.insertAdjacentHTML('beforeend', taskNode);
   }
   if (createTask.dataset.condition === 'save') {
-    //change task view
-    item__title.textContent = titleModalValue;
-    item__description.textContent = descriptionModalValue;
-    item__prior.textContent = priorityModalValue;
+    // change task view
+    itemTitle.textContent = titleModalValue;
+    itemDescription.textContent = descriptionModalValue;
+    itemPrior.textContent = priorityModalValue;
   }
   hideModal();
 });
-
-
-tasks.addEventListener('click', function (event) {
-  let target = event.target;
-
+tasks.addEventListener('click', (event) => {
+  const { target } = event;
   event.preventDefault();
   if (target.className === 'showConfig') {
     tasks.querySelectorAll('.showPop')
-      .forEach(element => {
-        element.style.display = 'none';
-      });//hide configModal if clicking on another edit button
+        .forEach((element) => {
+          const elem = element;
+          elem.style.display = 'none';
+        });// hide configModal if clicking on another edit button
     target.querySelector('.showPop').style.display = 'block';
-
   }
   if (target.className === 'ok') {
     target.closest('.tasks__item').style.backgroundColor = 'blue';
     target.closest('.tasks__item').dataset.id = 'done';
-    //rework event
-
+    // rework event
   }
   if (target.className === 'del') {
     target.closest('.tasks__item')
-      .remove();
+        .remove();
   }
   if (target.className === 'edit') {
     setFlag('save');
-
-    //отслеживаю на каком обьекте задач был клик и записываю в глобальную переменную текущие значения полей
-
-    item__title = target.closest('.tasks__item')
-      .querySelector('.item__title');
-    item__description = target.closest('.tasks__item')
-      .querySelector('.item__description');
-    item__prior = target.closest('.tasks__item')
-      .querySelector('.item__prior');
-
-    //записываю в текущее модальное окно поля из глобальной переменной
-
-    titleModal.value = item__title.textContent;
-    titleDescription.value = item__description.textContent;
-
+    // looking for target obj and set values in global
+    itemTitle = target.closest('.tasks__item')
+        .querySelector('.itemTitle');
+    itemDescription = target.closest('.tasks__item')
+        .querySelector('.itemDescription');
+    itemPrior = target.closest('.tasks__item')
+        .querySelector('.itemPrior');
+    // записываю в текущее модальное окно поля из глобальной переменной
+    titleModal.value = itemTitle.textContent;
+    titleDescription.value = itemDescription.textContent;
     showModal();
-
   }
   target.closest('.showPop').style.display = 'none';
 });
-
-
-//NEW FILTER
-let searchFilter = document.getElementById('searchFilter');
-let statusFilter = document.getElementById('statusFilter');
-let priorityFilter = document.getElementById('priorityFilter');
-
-function filter() {
-  //get ARRAY with tasks DOM-objects
-  let ARRAY = document.querySelectorAll('.tasks__item');
-  //get value of input
-  /*
-    let searchFilter=document.getElementById("searchFilter");
-  */
-  let searchValue = searchFilter.value;
-  //get value of status select
-  /*
-    let statusFilter=document.getElementById('statusFilter');
-  */
-  let statusValue = statusFilter.value;
-  //get value of priority select
-  /*
-    let priorityFilter=document.getElementById('priorityFilter');
-  */
-  let priorityValue = priorityFilter.value;
-  //get values to compare with selects and inputs
-
-
-  ARRAY.forEach(element => {
-    //each element in array compare with values in tasks
-    if (element.querySelector('.item__title')
-        .innerHTML
-        .includes(searchValue) &&
-      (element.dataset.id === statusValue || statusValue === 'all') &&
-      (element.querySelector('.item__prior').innerHTML === priorityValue || priorityValue === 'all')) {
-      //do something
-      element.style.display = 'block';
-    } else {
-      element.style.display = 'none';
-    }
-  });
-}
-
-//
 searchFilter.oninput = filter;
 statusFilter.onchange = filter;
 priorityFilter.onchange = filter;
